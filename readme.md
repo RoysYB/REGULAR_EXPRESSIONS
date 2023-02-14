47be2567c06acc424a4ddf15e3be3459478a6566
Roys <roysyb@gmail.com>
2023-02-14 22:57:38 +0530

    First commit

--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
- Regular  Expressions with python
+ Regular  Expressions with python 
 
- Aiming  to provide basic understanding of regular expressions which is implemented using python 
+ here we use re module 
+
+ Regular expression is an expression which is used inorder to specify  a set of strings
+ suppose {room,room1,room2} are public rooms available instead of specifying each rooms  we can create an expression which  takes less space and can specify any  room 
+here room(1|2)? can be a regular expression where ?== multiply by 0 or 1 , (1|2)== either 1 or 2  
+so if we want to search room 2  we can  specify it by  room 2*1    where 2 is choosen from  1 or 2
+there can be multiple regular expression for a set of  elements 
+An example could be to check if in a given email field the users are entering valid emails which are in the valid email format   
---
+++ regex.py
@@ -0,0 +1,45 @@
+import re
+
+#creating pattern
+pattern=re.compile("expression")  #it is used  to compile  the given input("the regular expression") which is processed in form of strings  into a pattern object of regex 
+#the  format  is    re.compile("expression",flags)
+#where flags can be set as 
+p=re.compile("green",flags=re.I)
+#  which allows patterns of  upper and lower cases combined
+
+#matching 
+  #match(): checks only at the beginnig(by default)
+  #match("  ",pos,endpos)
+k=p.match("green lantern")  #match object is returned if a match is found
+print(type(k))
+print(k)
+print(k.span())#returns the index where the match of the input has been found
+l=pattern.match("first expression here")
+print(l)#this will retun none since no match was found because we used match function which checks the beginning
+d=pattern.match("first expression", pos=6, endpos=17)
+print(d)#pos and endpos specifies the index to begin and end search  endpos can be any big integer
+ 
+ #search(): doesnot depend on value of pos 
+
+search=pattern.search(" the  expression  is  here") 
+print(search)#match object is returned if the pattern is found
+
+
+ #findall(): returns all the strings same to that of the input
+ #findall(" " ,pos=<int> ,endpos=<int> )  
+findall=p.findall("that is a green car near to a green truck")
+print(findall) #returns the same substring  not an object
+
+digits=re.compile("\d")#\d represents all the digits
+findall=digits.findall("1 tree 3 boys 4 girls 5 cats")
+print(findall) #prints all the digits returned from the findall function
+
+
+ #finditer(" ", pos=<int>,endpos=<int>)  
+ #finds all  the same matching substrings and returns all of them as an iterator of the match object
+
+pattern=re.compile("red")
+finditer=pattern.finditer(" red roses are red  ")
+for i in finditer: #print all the substrings matching the input
+  print(i)
+
